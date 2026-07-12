7def48500d361971ff2621f7624061ee7c6ffa99
ItsKiara89 <kiara.890000@Gmail.com>
2026-07-12 22:22:17 +0330

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Watching Yuki",
         // Activity type number (0 = Playing).
-        type: 1,
+        type: 5,
       },
     ],
   },
